89092956199974df296d9a96cd56b4af53245cd9
matyaya76 <matyaya76@gmail.com>
2022-04-02 17:27:46 +0900

    [start]java基礎5章開始

---
+++ Chapter05.java
@@ -0,0 +1,12 @@
+public class Chapter05 {
+
+    public static void main(String[] args) {
+        String string1;
+        string1 = "abc";
+        System.out.println(string1 + "Number of characters" + string1.length());
+        string1 = "";
+        System.out.println(string1 + "Number of characters" + string1.length());
+        string1 = null;
+        System.out.println(string1 + "Number of characters" + string1.length());   
+    }
+}
