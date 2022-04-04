862c3e7aee52f7623925792018dc015868174653
matyaya76 <matyaya76@gmail.com>
2022-04-04 21:14:59 +0900

    [update]基礎7章更新

--- Chapter07.java
+++ Chapter07.java
@@ -1,3 +1,31 @@
+import java.time.Year;
+
 public class Chapter07 {
-  
+    public static void main(String[] args) {
+        int distance = 12;
+        if (distance < 5) {
+            System.out.println("very close");
+        }else if (distance < 10) {
+            System.out.println("is close");
+        } else if (distance < 15) {
+            System.out.println("far");
+        } else {
+            System.out.println("very far");
+        }
+
+        String color = "yellow";
+        switch (color) {
+            case "blue":
+                System.out.println("blue");
+                break;
+            case "yellow":
+                System.out.println("yellow");
+                break;
+            case "red":
+                System.out.println("red");
+                break;
+            default :
+                System.out.println("nocolor");
+        }
+    }
 }
