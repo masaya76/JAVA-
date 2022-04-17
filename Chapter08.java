1b3f65f9523fe69f7852fd61f5a3d84544d1cc62
matyaya76 <matyaya76@gmail.com>
2022-04-17 22:52:05 +0900

    [update]基礎10 章- throws学習中

--- Chapter08.java
+++ Chapter08.java
@@ -1,28 +1,19 @@
 public class Chapter08 {
     public static void main(String[] args) {
-        int number = 2;
-        while (number < 50) { 
-            number *= 2;
-            System.out.println("while01 = " + number);
-        }
-        
         int num = 1;
         while (num < 5) {
             System.out.println(num * num);
             num++;
         }
-
-        int array[] = {1, 4, 14, 25};
+        int array[] = { 1, 4, 14, 25 };
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
-
         for (int val : array) {
             if (val % 2 == 0) {
                 continue;
             }
             System.out.println(val);
         }
-
     }
 }
--- exception10.java
+++ exception10.java
@@ -1,22 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class exception10 {
     public static void main(String[] args) {
         try {
-            exception10.division(100, 0);
-        } catch (ArithmeticException e) {
-            System.out.println("ArirhmeticException Exception occured");
-        } catch (IllegalArgumentException e) {
-            System.out.println("IllegalArgumentException Exception occured");
-            throw e;
+            exception10.readFile("exception.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException exception occurd");
         }
         System.out.println("The end of the program");
     }
 
-    public static void division(int a, int b) {
-        System.out.println(a + "/" + b + "ha?");
-        if (b == 0) {
-            throw new IllegalArgumentException("The argument is invalid");
-        } 
-        int result = a / b;
-        System.out.println("Calculation result" + result);
+    public static void readFile(String fileName) throws FileNotFoundException {
+        System.out.println("Start reading the file");
+        FileReader fr = new FileReader(fileName);
+        System.out.println(fileName + "reading comple");
     }
 }
