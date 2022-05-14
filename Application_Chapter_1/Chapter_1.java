b9a2d82b1e0a9e48cd2d27878c413e551088bbdb
matyaya76 <matyaya76@gmail.com>
2022-05-14 19:13:58 +0900

    [update] 画面表示方法学習中

--- Application_Chapter_1/Chapter_1.java
+++ Application_Chapter_1/Chapter_1.java
@@ -1,5 +1,24 @@
 package Application_Chapter_1;
 
-public class Chapter_1 {
-  
+import java.awt.*;
+import java.awt.event.*;
+
+public class Chapter_1 extends Frame {
+    public static void main(String[] args) {
+        Chapter_1 ch = new Chapter_1();
+    }
+
+    public Chapter_1() {
+        super("sample");
+        addWindowListener(new SampleWindowListenere());
+
+        setSize(250, 200);
+        setVisible(true);
+    }
+
+    class SampleWindowListenere extends WindowAdapter {
+        public void windowwClosing(WindowEvent e) {
+            System.exit(0);
+        }
+    }
 }
