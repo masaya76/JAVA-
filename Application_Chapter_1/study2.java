931e29dce1f3841f2aa96a5bf0184e2c82253a78
matyaya76 <matyaya76@gmail.com>
2022-05-20 08:12:02 +0900

    [update]マウスを使ってのGUI

--- Application_Chapter_1/study2.java
+++ Application_Chapter_1/study2.java
@@ -1,5 +1,38 @@
 package Application_Chapter_1;
 
-public class study2 {
-  
+import java.awt.*;
+import java.awt.event.*;
+
+public class study2 extends Frame {
+
+    private Button bt;
+
+    public static void main(String[] args) {
+        study2 st = new study2();
+    }
+
+    public study2() {
+        super("sample");
+
+        bt = new Button("welcome");
+        add(bt);
+
+        addWindowFocusListener(new a());
+        bt.addActionListener(new b());
+
+        setSize(250, 200);
+        setVisible(true);
+    }
+
+    public class a extends WindowAdapter {
+        public void windowClose(WindowEvent e) {
+            System.exit(0);
+        }
+    }
+
+    public class b implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            bt.setLabel("hello");
+        }
+    }
 }
--- Application_Chapter_1/study3.java
+++ Application_Chapter_1/study3.java
@@ -4,9 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class study3 extends Frame {
-
     private Button bt;
-
     public static void main(String[] args) {
         study3 st = new study3();
     }
@@ -14,25 +12,31 @@ public class study3 extends Frame {
     public study3() {
         super("sample");
 
-        bt = new Button("welcome");
+        bt = new Button("hello");
         add(bt);
 
-        addWindowFocusListener(new studyWindowListener());
-        bt.addActionListener(new studyActionListener());
+        addWindowListener(new a());
+        bt.addMouseListener(new b());
 
-        setSize(250, 200);
+        setSize(250,200);
         setVisible(true);
     }
 
-    public class studyWindowListener extends WindowAdapter {
-        public void windowClose(WindowEvent e) {
-            System.exit(0);
+    public class a extends WindowAdapter {
+        public void WindowClosing(WindowEvent e) {
+          System.exit(0);
         }
     }
 
-    public class studyActionListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            bt.setLabel("hello");
+    public class  b implements MouseListener {
+        public void mouseClicked(MouseEvent e) {}
+        public void mouseReleased(MouseEvent e) {} 
+        public void mousePressed(MouseEvent e) {}
+        public void mouseEntered(MouseEvent e) {
+            bt.setLabel("welcome");
+        }
+        public void mouseExited(MouseEvent e) {
+            bt.setLabel("good morning");
         }
     }
 }
