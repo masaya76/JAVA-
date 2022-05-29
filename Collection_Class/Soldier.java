57e40f1ac2ce050ec750a402f087e5a0533eff82
matyaya76 <matyaya76@gmail.com>
2022-05-29 23:02:05 +0900

    [update]collction学習

---
+++ Collection_Class/Soldier.java
@@ -0,0 +1,9 @@
+package Collection_Class;
+
+public class Soldier implements Character {
+  public int attack() {
+      System.out.println("A");
+      return 100;
+  }
+  
+}
---
+++ Collection_Class/collection_Application.java
@@ -0,0 +1,22 @@
+package Collection_Class;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class collection_Application {
+    public static void main(String []args) {
+      List<Character> party = new ArrayList<Character>();
+      party.add(new Soldier());
+      party.add(new Playboy());
+
+      collection_Application.partyAttack(party);
+  }
+
+  public static void partyAttack(List<Character> party) {
+      for (Character character : party) {
+          int hp = character.attack();
+          System.out.println("" + hp);
+      }
+    }
+  
+}
