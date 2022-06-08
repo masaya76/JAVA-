146d42829ec1cfe3785b99321f0cf05ba0df57d8
matyaya76 <matyaya76@gmail.com>
2022-06-08 22:59:40 +0900

    [update]境界型パラメータ学習中

--- Generics/generics.java
+++ Generics/generics.java
@@ -1,41 +1,30 @@
 package Generics;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class generics { 
-    public static void main(String[] args) {
-        List<Apple> list = new ArrayList<Apple>() {
-            {
-                add("window");
-                add("mac");
-                add("linux");
-            }
-        }
-    };
-
-    class Apple {
-        private String name;
-
-        Apple(String name) {
-            Apple appleBasket = new Apple("fuzi");
-            System.out.println(appleBasket.getName());
-        }
-
-        String getName() {
-            return name;
-        }
+class generics<T> {
+    private T value;
+ 
+    public generics(T val){
+        this.value = val;
+    }
+ 
+    public T getValue(){
+        return value;
     }
 
-    class Peach {
-        private String name;
-
-        Peach(String name) {
-            this.name = name;
+    public class generics {
+        public static void main(String[] args) {
+            // String型のジェネリクス
+            generics<String> cstr = new generics<String>("Hello ポテパン！");
+            String str = cstr.getValue();
+            System.out.println(str);
+    
+            // Integer型のジェネリクス
+            generics<Integer> cint = new generics<Integer>(100);
+            Integer inum = cint.getValue();
+            System.out.println(inum);
         }
-
-        String getName() {
-            return name;
-        }
-
     }
 }
