c51d68f85061d9c1de2616db52dd11ebafa70977
matyaya76 <matyaya76@gmail.com>
2022-06-19 23:59:19 +0900

    [start]High&Low - Deck.java,memoの開始

---
+++ High&Low/Deck.java
@@ -0,0 +1,15 @@
+package High&Low;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+class Card{
+    int suit;
+    int no;
+}
+
+public class Deck {
+  
+}
