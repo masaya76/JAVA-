a83c532b5e09545bb73d8cf7526d233836c80299
matyaya76 <matyaya76@gmail.com>
2022-06-23 22:58:00 +0900

    [update]HighLow - Playerクラス作成、Cardクラスは必要ないため削除

--- HighLow/Player.java
+++ HighLow/Player.java
@@ -1,10 +1,26 @@
 package HighLow;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 public class Player {
-  
+
+    private Card card_info;
+
+    public Player() {
+        card_info = new Card();
+        card_info.no = 0;
+        card_info.suit = 0;
+    }
+ 
+    public void Draw(Deck deck){
+        card_info = deck.GetCard();
+        return;
+    }
+
+    public int GetNo(){
+        return card_info.no;
+    }
+
+    public int GetSuit(){
+        return card_info.suit;
+    }
+
 }
