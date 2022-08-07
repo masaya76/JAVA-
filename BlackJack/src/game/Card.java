b17a0c14389c3f252a79a7ae5d483798cf83aa58
matyaya76 <matyaya76@gmail.com>
2022-08-07 23:06:44 +0900

    [update]BlackJack - 山札クラスのメンバ変数の決定

--- BlackJack/src/game/Card.java
+++ BlackJack/src/game/Card.java
@@ -1,5 +1,14 @@
 package game;
 
-public class Card {
+import java.util.ArraysList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
+public class Card {
+  int mark;
+  int number;
+  final int Toatal_Number = 52;
+  private List all_card;
+  
 }
